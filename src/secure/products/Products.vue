b050d0c78809cc908625e380dc10a5b23ff7845b
bilalbash <bilal.basharat@gmail.com>
2021-09-29 12:21:52 +0500

    f: products listing and deleting a product

---
+++ src/secure/products/Products.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="table-responsive">
+    <table class="table table-striped table-sm">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Image</th>
+          <th>Title</th>
+          <th>Description</th>
+          <th>Price</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="product in products" :key="product.id">
+          <td>{{ product.id }}</td>
+          <td><img :src="product.image" height="50" alt="" /></td>
+          <td>{{ product.title }}</td>
+          <td>{{ product.description }}</td>
+          <td>{{ product.price }}</td>
+          <td>
+            <div class="btn-group mr-2">
+              <router-link
+                :to="`/products/${product.id}/edit`"
+                class="btn btn-sm btn-outline-secondary">
+                Edit
+              </router-link>
+              <a
+                href="javascript:void(0)"
+                class="btn btn-sm btn-outline-secondary"
+                @click="deleteProduct(product.id)">
+                Delete
+              </a>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref, onMounted } from "vue";
+import axios from "axios";
+import { Entity } from "@/interfaces/entity";
+
+export default {
+  name: "Products",
+  setup() {
+    const products = ref([]);
+
+    onMounted(async () => {
+      const response = await axios.get("products");
+      products.value = response.data.data;
+    });
+
+    const deleteProduct = async (id: number) => {
+      if (confirm("Are u sure to del?")) {
+        await axios.delete(`products/${id}`);
+        products.value = products.value.filter((e: Entity) => e.id !== id);
+      }
+    };
+
+    return {
+      products,
+      deleteProduct,
+    };
+  },
+};
+</script>
