68fa0fb2e9b718e913cfce7a1d7353e7f4cbb0d5
bilalbash <bilal.basharat@gmail.com>
2021-09-30 11:46:38 +0500

    f: handling change event for product image upload
    t: handling change event for file upload, use of $event.target.files,

--- src/secure/products/ProductsCreate.vue
+++ src/secure/products/ProductsCreate.vue
@@ -19,7 +19,13 @@
 
     <div class="form-group">
       <label for="">Image</label>
-      <input type="text" class="form-control" name="Image" v-model="image" />
+      <div class="input-group-append">
+        <input type="text" class="form-control" name="Image" v-model="image" />
+        <label class="btn btn-primary">
+          Upload
+          <input type="file" hidden @change="change($event.target.files)" />
+        </label>
+      </div>
     </div>
 
     <div class="form-group">
@@ -31,7 +37,7 @@
   </form>
 </template>
 
-<script>
+<script lang="ts">
 import { ref } from "vue";
 import axios from "axios";
 import { useRouter } from "vue-router";
@@ -56,12 +62,23 @@ export default {
       await router.push("/products");
     };
 
+    const change = async (files: FileList) => {
+      const file = files.item(0);
+
+      const data = new FormData();
+      data.append("image", file);
+
+      const response = await axios.post("upload", data);
+      image.value = response.data.url;
+    };
+
     return {
       title,
       description,
       image,
       price,
       submit,
+      change,
     };
   },
 };
